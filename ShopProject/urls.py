33b946ac2bf3ac9b70c3717b25bbe2f62f8a6be7
Aleksandr Shishko <ghostas@tut.by>
2021-02-28 23:03:50 +0300

    Update Shop 3

--- ShopProject/urls.py
+++ ShopProject/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('main/api/v1', include('shop.urls')),
     path('admin/', admin.site.urls),
 ]
